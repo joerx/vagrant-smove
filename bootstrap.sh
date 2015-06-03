bbc8e734fcc63a36552ede68e1eef05f777403b2
Joerg Henning <henning.joerg@gmail.com>
2015-06-03 18:53:56 +0800

    Install node via nvm

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 export DEBIAN_FRONTEND=noninteractive
 
 echo -e "\e[0;36m[Upgrading system]\e[0m"
-apt-get -qq update
-apt-get -yqq upgrade
+# apt-get -qq update
+# apt-get -yqq upgrade
 
 if [ ! -f /usr/bin/curl ]; then
   echo -e "\e[0;36m[Installing curl]\e[0m"
@@ -17,12 +17,6 @@ fi
 if [ ! -f /usr/bin/g++ ]; then
   echo -e "\e[0;36m[Installing build-essential]\e[0m"
   apt-get -yqq install build-essential
-fi
-
-if [ ! -f /usr/bin/node ]; then
-  echo -e "\e[0;36m[Installing node]\e[0m"
-  curl -sL https://deb.nodesource.com/setup_0.12 | bash -
-  apt-get -y install nodejs
 fi
 
 if [ ! -f /usr/bin/mysql ]; then
@@ -45,21 +39,6 @@ if [ ! -f /etc/init.d/apache2 ]; then
   apt-get -yqq install apache2 php5 php5-cli phpmyadmin
 fi
 
-if [ ! -f /usr/bin/grunt ]; then
-  echo -e "\e[0;36m[Installing grunt]\e[0m"
-  npm -g --silent install grunt-cli
-fi
-
-if [ ! -f /usr/bin/sails ]; then
-  echo -e "\e[0;36m[Installing sails]\e[0m"
-  npm -g --silent install sails
-fi
-
-if [ ! -f /usr/bin/bower ]; then
-  echo -e "\e[0;36m[Installing bower]\e[0m"
-  npm -g --silent install bower
-fi
-
 if grep --quiet Singapore /etc/timezone; then
   echo -e "Timezone is ok"
 else 
@@ -67,3 +46,48 @@ else
   echo "Asia/Singapore" | sudo tee /etc/timezone
   dpkg-reconfigure --frontend noninteractive tzdata
 fi
+
+if [ ! -d /home/vagrant/.nvm ]; then
+  echo -e "\e[0;36m[Installing nvm]\e[0m"
+  if [ ! -f /usr/bin/node ]; then
+    echo -e "Removing npm from apt-get"
+    apt-get remove nodejs npm && apt-get autoremove
+    rm -f /etc/apt/sources.list.d/nodesource.list
+  fi
+  export HOME=/home/vagrant
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash
+  echo "source ~/.nvm/nvm.sh" >> /home/vagrant/.bashrc
+  source /home/vagrant/.nvm/nvm.sh
+  nvm install 0.10 > /dev/null 2> /dev/null
+  nvm install 0.12 > /dev/null 2> /dev/null
+  chown -R vagrant:vagrant /home/vagrant/.nvm
+  echo "Using node `nvm current`"
+  export HOME=/home/root
+fi
+
+
+function npm_globals {
+  source /home/vagrant/.nvm/nvm.sh
+  nvm use $1
+
+  if ! type grunt; then
+    echo -e "\e[0;36m[Installing grunt for $1]\e[0m"
+    npm -g --silent install grunt-cli
+    chown -R vagrant:vagrant /home/vagrant/.nvm
+  fi
+
+  if ! type sails; then
+    echo -e "\e[0;36m[Installing sails for $1]\e[0m"
+    npm -g --silent install sails
+    chown -R vagrant:vagrant /home/vagrant/.nvm
+  fi
+
+  if ! type bower; then
+    echo -e "\e[0;36m[Installing bower for $1]\e[0m"
+    npm -g --silent install bower
+    chown -R vagrant:vagrant /home/vagrant/.nvm
+  fi
+}
+
+npm_globals 0.10
+npm_globals 0.12
